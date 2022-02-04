4553647fccfb4440aa9d31292039eda13d6fc70f
Jian Ritik <jain.19@iitj.ac.in>
2022-02-04 19:23:33 +0530

    Removed comments in Url.js

--- api/models/Url.js
+++ api/models/Url.js
@@ -19,13 +19,3 @@ const Url = sequelize.define('Url', {
 
 module.exports = Url;
 
-// urlSchema.pre('save', function (next) {
-//   var doc = this;
-//   counter.findByIdAndUpdate({ _id: 'url_count' }, { $inc: { seq: 1 } }, function (error, counter) {
-//     if (error)
-//       return next(error);
-//     doc.created_at = new Date();
-//     doc._id = counter.seq;
-//     next();
-//   });
-// });
